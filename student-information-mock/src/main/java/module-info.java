82f99880e6681d79950d64b7a91b8507b0b3d062
JustineDaveMagnaye <157860693+JustineDaveMagnaye@users.noreply.github.com>
2024-03-15 22:20:30 +0700

    Added Magnaye Student Mock

---
+++ student-information-mock/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.video.rental.app {
+
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.student.information.mock to javafx.fxml;
+    opens com.student.information.mock.controllers.main to javafx.fxml;
+    opens com.student.information.mock.controllers.dashboard to javafx.fxml;
+
+    exports com.student.information.mock;
+    exports com.student.information.mock.controllers.main;
+    exports com.student.information.mock.controllers.dashboard;
+}
